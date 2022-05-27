003b9655661c61bc6161af5985ecadec9a708a47
Usame Kilicbay <usame.kilicbay@gmail.com>
2022-05-27 12:10:33 +0300

    feat: create and fill AppDbContext

---
+++ AppDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TodoneAPI.EntityConfigurations;
+
+namespace TodoneAPI
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> dbContextOptions) : base(dbContextOptions)
+        {
+
+        }
+
+        public DbSet<Todo> Todo { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasPostgresExtension("uuid-ossp");
+
+            modelBuilder.ApplyConfiguration(new TodoEntityTypeConfiguration());
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
