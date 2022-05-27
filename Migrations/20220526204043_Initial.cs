51f212a785383118d87f09cd7bc164995ae5632f
Usame Kilicbay <usame.kilicbay@gmail.com>
2022-05-27 12:10:49 +0300

    feat: add migrations

---
+++ Migrations/20220526204043_Initial.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TodoneAPI.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Todo",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "text", nullable: false),
+                    Mission = table.Column<string>(type: "text", nullable: false),
+                    MissionStatus = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Todo", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Todo");
+        }
+    }
+}
---
+++ Migrations/20220527082928_UpdateTodoIdColumnAsUUID.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TodoneAPI.Migrations
+{
+    public partial class UpdateTodoIdColumnAsUUID : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "Todo",
+                newName: "identifier");
+
+            migrationBuilder.AlterDatabase()
+                .Annotation("Npgsql:PostgresExtension:uuid-ossp", ",,");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "identifier",
+                table: "Todo",
+                type: "varchar(36)",
+                nullable: false,
+                defaultValueSql: "uuid_generate_v4()",
+                oldClrType: typeof(string),
+                oldType: "text");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "identifier",
+                table: "Todo",
+                newName: "Id");
+
+            migrationBuilder.AlterDatabase()
+                .OldAnnotation("Npgsql:PostgresExtension:uuid-ossp", ",,");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Id",
+                table: "Todo",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(36)",
+                oldDefaultValueSql: "uuid_generate_v4()");
+        }
+    }
+}
