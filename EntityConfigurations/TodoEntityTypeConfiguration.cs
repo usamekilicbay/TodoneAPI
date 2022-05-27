62e88b57c7efc711d844e44608764239ce4d5d68
Usame Kilicbay <usame.kilicbay@gmail.com>
2022-05-27 12:10:06 +0300

    feat: TodoEntityTrypeConfiguration

---
+++ EntityConfigurations/TodoEntityTypeConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace TodoneAPI.EntityConfigurations
+{
+    public class TodoEntityTypeConfiguration : IEntityTypeConfiguration<Todo>
+    {
+        public void Configure(EntityTypeBuilder<Todo> builder)
+        {
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Id)
+                   .HasColumnName("identifier")
+                   .HasColumnType("varchar(36)")
+                   .HasDefaultValueSql("uuid_generate_v4()")
+                   .IsRequired();
+        }
+    }
+}
