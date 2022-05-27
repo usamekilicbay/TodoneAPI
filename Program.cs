55abc9e5175d896bd6976a22c7f03315322bfc9d
Usame Kilicbay <usame.kilicbay@gmail.com>
2022-05-27 12:15:01 +0300

    feat: add API end point methods to Program.cs

--- Program.cs
+++ Program.cs
@@ -20,4 +20,37 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.MapGet("/api/todos", async (AppDbContext appDbContext) => await appDbContext.Todo.ToListAsync());
+
+app.MapGet("/api/todos/{id}", async (AppDbContext appDbContext, string id) => await appDbContext.Todo.FindAsync(id));
+
+app.MapPost("/api/todos", async (AppDbContext appDbContext, Todo todo) =>
+{
+    await appDbContext.Todo.AddAsync(todo);
+    await appDbContext.SaveChangesAsync();
+    Results.Accepted();
+});
+
+app.MapPut("/api/todos/{id}", async (AppDbContext appDbContext, Todo todo, string id) =>
+{
+    if (id != todo.Id) Results.BadRequest();
+
+    appDbContext.Todo.Update(todo);
+    await appDbContext.SaveChangesAsync();
+
+    return Results.NoContent();
+});
+
+app.MapDelete("/api/todos/{id}", async (AppDbContext appDbContext, string id) =>
+{
+    var todo = await appDbContext.Todo.FindAsync(id);
+
+    if (todo == null)
+        return Results.NotFound();
+
+    appDbContext.Todo.Remove(todo);
+    await appDbContext.SaveChangesAsync();
+    return Results.NoContent();
+});
+
 app.Run();
