308f17b5fdeb0cec2a2b165de47ce781dc245b1b
Usame Kilicbay <usame.kilicbay@gmail.com>
2022-05-27 12:08:14 +0300

    feat: Todo entity

---
+++ Todo.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TodoneAPI
+{
+    public class Todo
+    {
+        [Key]
+        public string Id { get; set; }
+        public string Mission { get; set; }
+        public MissionStatus MissionStatus { get; set; }
+    }
+
+    public enum MissionStatus
+    {
+        TODO,
+        IN_PROCESS,
+        DONE
+    }
+}
